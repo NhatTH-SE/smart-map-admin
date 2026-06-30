51b9ac043477d67498d52293565d09fe88699970
NhatTH-SE <nhatth.ce190581@gmail.com>
2026-06-30 17:03:18 +0700

    Init project: Setup Spring Boot & ReactJS Vite

---
+++ smart_map/src/main/java/com/fpt/edu/vn/smart_map/SmartMapApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.vn.smart_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMapApplication.class, args);
+    }
+
+}
---
+++ smart_map/src/test/java/com/fpt/edu/vn/smart_map/SmartMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.edu.vn.smart_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
